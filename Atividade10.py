7d476c29c7ee607bed097085435c66071d7c6dbc
Arthur TurmaD tarde <arthur28lucas@gmail.com>
2023-04-13 17:18:46 -0300

    Atividades python CDD4.0

--- Atividade10.py
+++ Atividade10.py
@@ -4,11 +4,9 @@ senhausuario=str(input("Digite sua senha: "))
 
 if senhausuario!=senha:
     while senhausuario!=senha and contadorban!=3:
-
         print("SENHA INCORRETA\n Tente novamente")
         contban=contadorban+1
         senhausuario = str(input("Digite sua senha: "))
-
         if contadorban==3:
             print("\033[1;31mVoce foi BANIDO\033[m")
         else:
