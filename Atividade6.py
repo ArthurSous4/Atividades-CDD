673fc11b683e6ed9d83772b329c142fb02be808b
Arthur TurmaD tarde <arthur28lucas@gmail.com>
2023-04-13 16:11:40 -0300

    Atividades python CDD4.0 13/

---
+++ Atividade6.py
@@ -0,0 +1,9 @@
+n=int(input("digite um número: "))
+soma=0
+i=0
+
+while i<= n:
+    soma+=1
+    i=i+1
+
+print("A soma é:",soma)
---
+++ Atividade7.py
@@ -0,0 +1,9 @@
+cont=0
+soma=0
+
+while cont!=10:
+    nota=float(input("digite sua nota: "))
+    soma=soma+nota
+    cont=cont+1
+
+print("sua média foi: ",soma/10)
